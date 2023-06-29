8870153c098b2771eb942a19319e432967a7dbb3
Vincent Song <dela.vincent.15@gmail.com>
2023-06-28 22:34:57 -0400

    Add playRound function

--- rps.js
+++ rps.js
@@ -4,4 +4,40 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    var player = playerSelection.toLowerCase();
+    var computer = computerSelection.toLowerCase();
+
+    if(player === computer){
+        return "Tie!"
+    }
+    if(player === "rock"){
+        if(computer === "paper"){
+            return "You Lose! Paper beats Rock";
+        }
+        if(computer === "scissors"){
+            return "You Win! Rock beats Scissors";
+        }
+    }
+    if(player === "paper"){
+        if(computer === "rock"){
+            return "You Win! Paper beats Rock";
+        }
+        if(computer === "scissors"){
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+    if(player === "scissors"){
+        if(computer === "rock"){
+            return "You Lose! Rock beats Scissors";
+        }
+        if(computer === "paper"){
+            return "You Win! Scissors beats Paper";
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
