ace3c61789a433416246fd09b437ffae0951e2fb
Vincent Song <dela.vincent.15@gmail.com>
2023-08-01 22:46:01 -0400

    Finish UI

--- rps.js
+++ rps.js
@@ -1,78 +1,91 @@
+let playerScore = 0;
+let computerScore = 0;
+updateScore();
+
 function getComputerChoice() {
-    var choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     const choice = choices[Math.floor(Math.random() * choices.length)];
     return choice;
 }
 
-function playRound(playerSelection, computerSelection){
-    var player = playerSelection.toLowerCase();
-    var computer = computerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
 
-    if(player === computer){
-        return "Tie!"
-    }
-    if(player === "rock"){
-        if(computer === "paper"){
+    if (player === computer) {
+        return "Tie!";
+    } else if (player === "rock") {
+        if (computer === "paper") {
+            computerScore++;
             return "You Lose! Paper beats Rock";
-        }
-        if(computer === "scissors"){
+        } else if (computer === "scissors") {
+            playerScore++;
             return "You Win! Rock beats Scissors";
         }
-    }
-    if(player === "paper"){
-        if(computer === "rock"){
+    } else if (player === "paper") {
+        if (computer === "rock") {
+            playerScore++;
             return "You Win! Paper beats Rock";
-        }
-        if(computer === "scissors"){
+        } else if (computer === "scissors") {
+            computerScore++;
             return "You Lose! Scissors beats Paper";
         }
-    }
-    if(player === "scissors"){
-        if(computer === "rock"){
+    } else if (player === "scissors") {
+        if (computer === "rock") {
+            computerScore++;
             return "You Lose! Rock beats Scissors";
-        }
-        if(computer === "paper"){
+        } else if (computer === "paper") {
+            playerScore++;
             return "You Win! Scissors beats Paper";
         }
     }
 }
 
-function game(){
-    var playerScore = 0;
-    var computerScore = 0;
+function updateScore() {
+    const scoreElement = document.getElementById("results");
+    scoreElement.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 
-    for(let i = 0; i < 5; i++){
-        var round = i + 1;
-        console.log("Round ", round.toString());
+    if (playerScore === 5 || computerScore === 5) {
+        const winner =
+            playerScore === 5 ? "Player" : computerScore === 5 ? "Computer" : "No one";
+        scoreElement.textContent = `Game Over! ${winner} wins the game.`;
 
-        const playerSelection = prompt("Rock, Paper, or Scissors?")
-        const computerSelection = getComputerChoice();
-
-        console.log("Computer chose:", computerSelection)
-
-        var result = playRound(playerSelection, computerSelection) 
-        if(result.includes("Win")){
-            playerScore++;
-        }
-        else{
-            computerScore++;
-        }
-        console.log(result);
-    }
-
-    
-    console.log("Game Over");
-    console.log("Your Score: ", playerScore.toString());
-    console.log("Computer Score: ", computerScore.toString());
-    if(playerScore > computerScore){
-        console.log("You beat the computer! Good job!");
-    }
-    else if (playerScore < computerScore){
-        console.log("You lost to the computer! Try Again!");
-    }
-    else{
-        console.log("You tied with the computer. Not bad!")
+        // Disable the buttons once the game is over.
+        document.getElementById("rock").disabled = true;
+        document.getElementById("paper").disabled = true;
+        document.getElementById("scissors").disabled = true;
+        document.getElementById("restart").disabled = false;
     }
 }
 
-game();
+function onButtonClick(event) {
+    const playerSelection = event.target.textContent;
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    const resultElement = document.getElementById("results");
+    resultElement.textContent = result;
+    updateScore();
+}
+
+function onRestartClick() {
+    playerScore = 0;
+    computerScore = 0;
+    const resultElement = document.getElementById("results");
+    resultElement.textContent = "";
+    updateScore();
+
+    // Enable the buttons again for the next game.
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissors").disabled = false;
+    document.getElementById("restart").disabled = true;
+}
+
+// Add event listeners to buttons
+document.getElementById("rock").addEventListener("click", onButtonClick);
+document.getElementById("paper").addEventListener("click", onButtonClick);
+document.getElementById("scissors").addEventListener("click", onButtonClick);
+document.getElementById("restart").addEventListener("click", onRestartClick);
+
+// Disable the restart button initially until the game ends.
+document.getElementById("restart").disabled = true;
